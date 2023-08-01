105922e2d61f9ca2c7d3872ba4fee8c4ecd0fb24
Kaleb Asfaw <122475977+kaleb-asfaw@users.noreply.github.com>
2023-08-01 11:08:20 -0700

    Update run_interpreter.py

--- run_interpreter.py
+++ run_interpreter.py
@@ -41,35 +41,5 @@ def interpreter(verbose=False):
             print("Error>", repr(e))
 
 if __name__ == "__main__":
-    # code in this block will only be executed if lab.py is the main file being
-    # run (not when this module is imported)
-
     interpreter(True)
-    # source = "(define circle-area (lambda (r) (* 3.14 (* r r))))"
-    # source2 = '(w3!|]\\d5y8[]1$)'
-    # source3 = '(multi\nline\nstatement)'
-    # source4 = '(multiline ; with comments\n statement) ; with comments'
-    # source5 = "(cat (dog (tomato)))"
-    # tokens = tokenize(source)
-    # print(tokens)
-    # print(parse(tokenize(source)))
-    # print(evaluate(['+', 3, ['-', 7, 5]]))
-    # print(result_and_frame([['lambda', ['x', 'y', 'z'], ['+', ['*', 'x', 'x'], ['*', 'y', 'y'], ['*', 'z', 'z']]], 7, 8, 9]))
-    # print(evaluate(['and', ['>', 3, 2], ['not', ['equal?', 3, 4]] ,['<', 7, 8, 9], '#t', ["equal?", 1, 0, 0, 0]]))
-    # blud = 5
-    # x = ["cons", 1, ["cons", 2, ["cons", 3, ["cons", blud, "nil"]]]]
-    # x = ["cons", 1, 2]
-    # l = 'nil'
-    # L = evaluate(l)
-    # # print(list_len('nil'))
-    # hi = evaluate(x)
-    # print(hi)
-    # print(check_list(x))
-    # # print(list_len(hi))
-    # # print(get_val_at_i(hi, 2))
-    # # print(listt([1,2,3,4,3]))
-    # l1 = listt([1,2, 3, 4, 5])
-    # l2 = listt([6, 7, 8, 9])
-    # print(check_list(l1))
-    # print(check_list(hi))
-    # print(get_val_at_i(hi, 0))
+    
